44545a9b582a488a7e5174969e2942e203c43f97
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2022-09-28 03:20:01 -0300

    Added dependency injection

--- SchoolApp.File.Api/Program.cs
+++ SchoolApp.File.Api/Program.cs
@@ -1,11 +1,57 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SchoolApp.File.Application.Interfaces.Repositories;
+using SchoolApp.File.Application.Interfaces.Services;
+using SchoolApp.File.Application.Services;
+using SchoolApp.File.Blob.Repositories;
+using SchoolApp.File.Blob.Settings;
+using SchoolApp.File.Http.Repositories;
+using SchoolApp.File.Http.Settings;
+using SchoolApp.Shared.Utils.HttpApi.Extensions;
+using SchoolApp.Shared.Utils.HttpApi.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().HandleDataAnnotationExceptions();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IUserFileService, UserFileService>();
+builder.Services.AddScoped<IStudentFileService, StudentFileService>();
+builder.Services.AddScoped<IActivityFileService, ActivityFileService>();
+builder.Services.AddScoped<IActivityAnswerVersionFileService, ActivityAnswerVersionFileService>();
+
+builder.Services.AddScoped(typeof(IFileRepository<>), typeof(FileRepository<>));
+builder.Services.AddHttpClient<IStudentRepository, StudentRepository>();
+builder.Services.AddHttpClient<IClassroomRepository, ClassroomRepository>();
+builder.Services.AddHttpClient<IActivityAnswerVersionRepository, ActivityAnswerVersionRepository>();
+builder.Services.AddHttpClient<IActivityRepository, ActivityRepository>();
+
+builder.Services.Configure<AzureBlobStorageSettings>(builder.Configuration.GetSection(nameof(AzureBlobStorageSettings)));
+builder.Services.Configure<ActivityServiceApiSettings>(builder.Configuration.GetSection(nameof(ActivityServiceApiSettings)));
+builder.Services.Configure<ClassroomServiceApiSettings>(builder.Configuration.GetSection(nameof(ClassroomServiceApiSettings)));
+
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["AuthenticationSettings:Issuer"],
+            ValidAudience = builder.Configuration["AuthenticationSettings:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AuthenticationSettings:Key"]))
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,6 +64,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
